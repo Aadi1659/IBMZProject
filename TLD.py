0991e2e1aba6adc03116d14534abca47e28473f6
crazydj8 <akshataryan8@gmail.com>
2022-09-24 12:26:59 +0530

    changed webdriver to firefox. updated code to not open browser for webscraping

--- TLD.py
+++ TLD.py
@@ -1,22 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.firefox.options import Options
 import pandas as pd
 
 
 #added a comment
 website = 'https://data.iana.org/TLD/tlds-alpha-by-domain.txt'
-options=webdriver.ChromeOptions()
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver = webdriver.Chrome(options=options)
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Firefox(options=options)
 driver.get(website)
-driver.maximize_window()
+#driver.maximize_window()
 element=driver.find_element(By.XPATH, '/html/body/pre')
 TLD=element.text
 TLD=TLD.split('\n')[1:]
 driver.quit()
 database=pd.DataFrame(dict(TLDs=TLD))
-database.to_csv('C:\\Users\\OmiIn\\PycharmProjects\\pythonProject\\TLD.csv')
+database.to_csv('TLD.csv')
 print(TLD)
 
 
